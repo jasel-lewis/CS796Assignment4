14047335fa8f2cd0b2e5c8b604e8a6d0b3421d3b
Jasel <jasel@jasel.com>
2013-11-25 02:04:48 -0500

    Added usage and command-line argument error handlers

--- src/com/jasel/classes/cs796/assignment4/unoserver.java
+++ src/com/jasel/classes/cs796/assignment4/unoserver.java
@@ -13,7 +13,36 @@ public class unoserver {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int port;
+		
+		if (args.length == 1) {
+			try {
+				port = Integer.parseInt(args[0]);
+			} catch (NumberFormatException nfe) {
+				handlePortError();
+			}
+		} else {
+			printUsage(1);
+			
+		}
+	}
+	
+	
+	
+	private static void printUsage(int exitCode) {
+		System.out.println("UNOserver - Urgent/Normal On-demand Server");
+		System.out.println("Usage: unoserver <port>");
+		System.out.println("   - <port> The port on which to run the server (in " +
+				"the range 1024-65535)");
+		
+		System.exit(exitCode);
+	}
+	
+	
+	
+	private static void handlePortError() {
+		System.err.println("The sole argument <port> must be an integer in the " +
+				"range 1024 to 65535");
+		System.exit(1);
 	}
 }
