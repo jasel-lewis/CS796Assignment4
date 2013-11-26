56cd85aa62eca9db462d783be597b75733af490b
jasel-lewis <jasel.lewis@gmail.com>
2013-11-25 23:02:32 -0500

    Realized no port is required on startup - Dr. Wahab must want us to set it via (application) runtime

--- src/com/jasel/classes/cs796/assignment4/unoserver.java
+++ src/com/jasel/classes/cs796/assignment4/unoserver.java
@@ -8,27 +8,12 @@ package com.jasel.classes.cs796.assignment4;
  *
  */
 public class unoserver {
-	static final int PORTMIN = 1024;
-	static final int PORTMAX = 65535;
-
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		int port = 0;
-		
-		if (args.length == 1) {
-			try {
-				port = Integer.parseInt(args[0]);
-			} catch (NumberFormatException nfe) {
-				handlePortError();
-			}
-		} else {
+		if (args.length != 0) {
 			printUsage(1);
-		}
-		
-		if ((port < PORTMIN) || (port > PORTMAX)) {
-			handlePortError();
 		}
 	}
 	
@@ -36,18 +21,8 @@ public class unoserver {
 	
 	private static void printUsage(int exitCode) {
 		System.out.println("UNOserver - Urgent/Normal On-demand Server");
-		System.out.println("Usage: unoserver <port>");
-		System.out.println("   - <port> The port on which to run the server (in " +
-				"the range " + PORTMIN + "-" + PORTMAX + ")");
+		System.out.println("Usage: unoserver\n");
 		
 		System.exit(exitCode);
-	}
-	
-	
-	
-	private static void handlePortError() {
-		System.err.println("The sole argument <port> must be an integer in the " +
-				"range " + PORTMIN + "-" + PORTMAX + ")");
-		System.exit(1);
 	}
 }
