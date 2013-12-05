f7952f8e88ed8c6e76170d9f1ad8c045e9a2305d
jasel-lewis <jasel.lewis@gmail.com>
2013-12-04 23:28:05 -0500

    Initial creation

---
+++ src/com/jasel/classes/cs796/assignment4/unoclient.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+package com.jasel.classes.cs796.assignment4;
+
+import java.awt.EventQueue;
+
+import com.jasel.classes.cs796.assignment4.controller.ClientController;
+import com.jasel.classes.cs796.assignment4.view.ClientView;
+import com.jasel.classes.cs796.assignment4.view.CloseListener;
+
+/**
+ * @author Jasel
+ */
+public class unoclient {
+	protected static final int DEFAULTPORT = 8008;
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		if (args.length != 0) {
+			printUsage(1);
+		}
+		
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ClientView view = new ClientView(DEFAULTPORT);
+					ClientController controller = new ClientController(view);
+					
+					view.setController(controller);
+					view.addWindowListener(new CloseListener("Exit UNOClient", "Are you sure you wish to exit UNOClient?"));
+					//view.pack();
+					view.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
+	
+	
+	private static void printUsage(int exitCode) {
+		System.out.println("UNOclient - Urgent/Normal On-demand Client");
+		System.out.println("Usage: unoclient\n");
+		
+		System.exit(exitCode);
+	}
+}
